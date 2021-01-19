ca343bcbc0ec00665d72bb16239c9371a5fe5423
Emmanuel <qamanny@academytrainee.com>
2021-01-19 19:37:08 +0000

    Seeing the differences between customer and items it might meanthat Utils needs the ability to read the new double value

--- src/main/java/com/qa/ims/utils/Utils.java
+++ src/main/java/com/qa/ims/utils/Utils.java
@@ -13,5 +13,11 @@ public class Utils {
 		Scanner scanner = new Scanner(System.in);
 		return scanner.nextLine();
 	}
+	
+	public static double getDouble() {
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextDouble();
+	}
 
 }

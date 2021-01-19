5a5ce020ac6759f6b8ae4e8470b31ed11915cce9
Emmanuel <qamanny@academytrainee.com>
2021-01-19 18:37:42 +0000

    Started working on ItemDao, somehow not importing right, come back to it later

--- src/main/java/com/qa/ims/persistence/dao/ItemDaoMysql.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemDaoMysql.java
@@ -1,5 +1,38 @@
 package com.qa.ims.persistence.dao;
 
-public class ItemDaoMysql {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Item;
+
+public class ItemDaoMysql implements Dao<Item> {
+	
+	public static final Logger LOGGER = Logger.getLogger(ItemDaoMysql.class);
+
+	private String jdbcConnectionUrl;
+	private String username;
+	private String password;
+
+	public ItemDaoMysql(String username, String password) {
+		this.jdbcConnectionUrl = "jdbc:mysql://localhost:3306/ims";
+		this.username = username;
+		this.password = password;
+
+	}
+	
+	public ItemDaoMysql(String jdbcConnectionUrl, String username, String password) {
+		this.jdbcConnectionUrl = jdbcConnectionUrl;
+		this.username = username;
+		this.password = password;
+	}
+	
+	Item itemFromResultSet(ResultSet resultSet) throws SQLException {
+		Long id = resultSet.getLong("id");
+		String name = resultSet.getString("item_name");
+		double price = resultSet.getDouble("item_price");
+		return new Item(id, name, price);
+	}
 
 }

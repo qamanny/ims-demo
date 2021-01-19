9eb665bc696a90a61d2e37f91fbde80339a18874
Emmanuel <qamanny@academytrainee.com>
2021-01-19 16:09:00 +0000

    Item Controller set and ready for something to control

--- src/main/java/com/qa/ims/controller/ItemController.java
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -1,5 +1,48 @@
 package com.qa.ims.controller;
 
-public class ItemController {
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.services.CrudServices;
+import com.qa.ims.utils.Utils;
+
+public class ItemController implements CrudController<Item>{
+	
+	public static final Logger LOGGER = Logger.getLogger(ItemController.class);
+	
+	private CrudServices<Item> itemService; {
+		this.itemService = itemService;
+	}
+	
+	
+	String getInput() {
+		return Utils.getInput();
+	}
+
+	@Override
+	public List<Item> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }

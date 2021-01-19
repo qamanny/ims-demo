836dba7c2089992060eca7faae8ea9e9a4b53036
Emmanuel <qamanny@academytrainee.com>
2021-01-19 19:42:47 +0000

    Made some changes to the schema to make way for items

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,2 +1,3 @@
 create database if not exists ims;
 create table if not exists ims.customer(id int primary key auto_increment, first_name varchar(40), surname varchar(40));
+create table if not exists ims.items(id int primary key auto_increment, item_name varchar(40), item_price double);

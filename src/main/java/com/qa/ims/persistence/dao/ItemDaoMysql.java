7f46a7a143e9a38099b99d1db72f85fb36956b37
Emmanuel <qamanny@academytrainee.com>
2021-01-19 21:42:26 +0000

    Fixed ItemDao and adjusted sql-schema a little

--- src/main/java/com/qa/ims/persistence/dao/ItemDaoMysql.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemDaoMysql.java
@@ -1,38 +1,128 @@
 package com.qa.ims.persistence.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.log4j.Logger;
-
 import com.qa.ims.persistence.domain.Item;
 
 public class ItemDaoMysql implements Dao<Item> {
 	
 	public static final Logger LOGGER = Logger.getLogger(ItemDaoMysql.class);
-
+	
 	private String jdbcConnectionUrl;
 	private String username;
 	private String password;
-
+	
 	public ItemDaoMysql(String username, String password) {
 		this.jdbcConnectionUrl = "jdbc:mysql://localhost:3306/ims";
 		this.username = username;
 		this.password = password;
-
 	}
-	
+
 	public ItemDaoMysql(String jdbcConnectionUrl, String username, String password) {
 		this.jdbcConnectionUrl = jdbcConnectionUrl;
 		this.username = username;
 		this.password = password;
 	}
-	
+
 	Item itemFromResultSet(ResultSet resultSet) throws SQLException {
 		Long id = resultSet.getLong("id");
-		String name = resultSet.getString("item_name");
-		double price = resultSet.getDouble("item_price");
-		return new Item(id, name, price);
+		String item_name = resultSet.getString("item_name");
+		double item_price = resultSet.getDouble("item_price");
+		return new Item(id, item_name, item_price);
 	}
 
+	@Override
+	public List<Item> readAll() {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("select * from item");) {
+			ArrayList<Item> item = new ArrayList<>();
+			while (resultSet.next()) {
+				item.add(itemFromResultSet(resultSet));
+			}
+			return item;
+		} catch (SQLException e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
+	}
+
+	public Item readLatest() {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM item ORDER BY id DESC LIMIT 1");) {
+			resultSet.next();
+			return itemFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+	
+	
+	@Override
+	public Item create(Item item) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("insert into customer(first_name, surname) values('" + item.getItem_name()
+					+ "','" + item.getItem_price() + "')");
+			return readLatest();
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+	
+	public Item readItem(Long id) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM item where id = " + id);) {
+			resultSet.next();
+			return itemFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+
+	@Override
+	public Item update(Item item) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("update item set item_name ='" + item.getItem_name() + "', price ='"
+					+ item.getItem_price() + "' where id =" + item.getId());
+			return readItem(item.getId());
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	@Override
+	public void delete(long id) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("delete from item where id = " + id);
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+	}
+	
+	
+	
+	
+	
 }

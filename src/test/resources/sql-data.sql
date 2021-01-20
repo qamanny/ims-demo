1e566a9e68bfefea8221c440c1bbe42bf57cfc8d
Emmanuel <qamanny@academytrainee.com>
2021-01-20 10:40:29 +0000

    Adjusted sql schema and data

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -2,4 +2,8 @@ insert into ims_test.customer(first_name, surname) values('chris', 'perrins');
 insert into ims_test.customer(first_name, surname) values('rhys', 'thompson');
 insert into ims_test.customer(first_name, surname) values('nic', 'johnson');
 insert into ims_test.customer(first_name, surname) values('jordon', 'harrison');
+insert into ims_test.item(item_name, item_price) values('Red Wrench', 18.99);
+insert into ims_test.item(item_name, item_price) values('Blue Wrench', 17.99);
+insert into ims_test.item(item_name, item_price) values('Orange Wrench', 16.99)
+insert into ims_test.item(item_name, item_price) values('Grey Wrench', 14.99)
 
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,3 +1,5 @@
 create database if not exists ims_test;
 drop table if exists ims_test.customer;
 create table ims_test.customer(id int primary key auto_increment, first_name varchar(40), surname varchar(40));
+create table ims_test.item(id int primary key auto_increment, item_name varchar(40), item_price double);
+

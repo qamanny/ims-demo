8b7fa09452159bbcdaed3444ef8a942d7355bcf6
Emmanuel <qamanny@academytrainee.com>
2021-01-20 11:50:18 +0000

    Tested ItemController and modified sql schema

--- src/test/java/com/qa/ims/controller/ItemControllerTest.java
+++ src/test/java/com/qa/ims/controller/ItemControllerTest.java
@@ -1,5 +1,86 @@
 package com.qa.ims.controller;
 
-public class ItemControllerTest {
+import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.services.ItemServices;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ItemControllerTest {
+	
+	/**
+	 *  The thing I want to fake functionality for
+	 */ 
+	@Mock
+	private ItemServices itemServices;
+	
+	/**
+	 * Spy is used because i want to mock some methods inside the item I'm testing
+	 * InjectMocks uses dependency injection to insert the mock into the customer controller
+	 */
+	@Spy
+	@InjectMocks
+	private ItemController itemController;
+
+	@Test
+	public void readAllTest() {
+		ItemController itemController = new ItemController(itemServices);
+		List<Item> items = new ArrayList<>();
+		items.add(new Item("Red Wrench", 18.99));
+		items.add(new Item("Blue Wrench", 17.99));
+		items.add(new Item("Orange Wrench", 16.99));
+		Mockito.when(itemServices.readAll()).thenReturn(items);
+		assertEquals(items, itemController.readAll());
+	}
+
+	@Test
+	public void createTest() {
+		String item_name = "Red Wrench";
+		Double item_price = 18.99;
+		Mockito.doReturn(item_name, item_price.toString()).when(itemController).getInput();
+		Item item = new Item(item_name, item_price);
+		Item savedItem = new Item(1L, "Red Wrench", 18.99);
+		Mockito.when(itemServices.create(item)).thenReturn(savedItem);
+		assertEquals(savedItem, itemController.create());
+	}
+
+	/**
+	 * 
+	 */
+	@Test
+	public void updateTest() {
+		String id = "1";
+		String item_name = "Blue Wrench";
+		Double item_price = 17.99;
+		Mockito.doReturn(id, item_name, item_price.toString()).when(itemController).getInput();
+		Item item = new Item(1L, item_name, item_price);
+		Mockito.when(itemServices.update(item)).thenReturn(item);
+		assertEquals(item, itemController.update());
+	}
+	
+
+	/**
+	 * Delete doesn't return anything, so we can just verify that it calls the delete method
+	 */
+	@Test
+	public void deleteTest() {
+		String id = "1";
+		Mockito.doReturn(id).when(itemController).getInput();
+		itemController.delete();
+		Mockito.verify(itemServices, Mockito.times(1)).delete(1L);
+	}
 }
+
+
+
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,5 +1,6 @@
 create database if not exists ims_test;
 drop table if exists ims_test.customer;
+drop table if exists ims_test.item;
 create table ims_test.customer(id int primary key auto_increment, first_name varchar(40), surname varchar(40));
 create table ims_test.item(id int primary key auto_increment, item_name varchar(40), item_price double);
 

93d86f860a5255868cf0027cf0f155de40dfb2d1
Emmanuel <qamanny@academytrainee.com>
2021-01-21 14:03:36 +0000

    Finished Orderline Services

--- src/main/java/com/qa/ims/services/OrderlineServices.java
+++ src/main/java/com/qa/ims/services/OrderlineServices.java
@@ -1,5 +1,34 @@
 package com.qa.ims.services;
 
-public class OrderlineServices {
+import java.util.List;
+
+import com.qa.ims.persistence.dao.Dao;
+import com.qa.ims.persistence.domain.Orderline;
+
+public class OrderlineServices implements CrudServices<Orderline> {
+	
+private Dao<Orderline> orderlineDao;
+	
+	public OrderlineServices(Dao<Orderline> orderlineDao) {
+		this.orderlineDao = orderlineDao;
+	}
+
+	public List<Orderline> readAll() {
+		return orderlineDao.readAll();
+	}
+
+	public Orderline create(Orderline orderline) {
+		return orderlineDao.create(orderline);
+	}
+
+	public Orderline update(Orderline orderline) {
+		return orderlineDao.update(orderline);
+	}
+
+	public void delete(Long id) {
+		orderlineDao.delete(id);
+		
+	}
+
 
 }

4b85a91cdf5da477313f8362a5d57e8c2750d276
Emmanuel <qamanny@academytrainee.com>
2021-01-22 07:32:09 +0000

    Literally the most basic functionality achieved, I'll try and up the level by say 9am giving me enough time to prep testing and practice for what will be an 'interesting' presentation. Ahhh Dios Mio

--- src/main/java/com/qa/ims/controller/OrderController.java
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 
+import com.qa.ims.persistence.domain.Item;
 import com.qa.ims.persistence.domain.Order;
 import com.qa.ims.persistence.domain.Orderline;
 import com.qa.ims.services.CrudServices;
@@ -15,42 +16,57 @@ public class OrderController implements CrudController<Order> {
 
 	private CrudServices<Order> orderService;
 
-	publ
-
+	public OrderController(CrudServices<Order> orderServices) {
+		this.orderService = orderServices;
+	}
+	
 	String getInput() {
 		return Utils.getInput();
-	}
-
-	Long getLong() {
-		return Utils.getLong();
 	}
 
 	@Override
 	public List<Order> readAll() {
 		List<Order> orders = orderService.readAll();
-		for (Order order : orders) {
+		for(Order order: orders) {
 			LOGGER.info(order.toString());
 		}
 		return orders;
-		
 	}
 
 	@Override
 	public Order create() {
-		// TODO Auto-generated method stub
-		return null;
+		LOGGER.info("Please enter the customer id");
+		Long customerId = Long.valueOf(getInput());
+		LOGGER.info("Please enter the address of the customer");
+		String address = getInput();
+		LOGGER.info("Please enter the postcode of the customer");
+		String postcode = getInput();
+		Order order = orderService.create(new Order(customerId, address, postcode));
+		LOGGER.info("Item created");
+		return order;
 	}
 
 	@Override
 	public Order update() {
-		// TODO Auto-generated method stub
-		return null;
+		LOGGER.info("Please enter the id of the order you would like to update");
+		Long orderId = Long.valueOf(getInput());
+		LOGGER.info("Please enter the customer id");
+		Long customerId = Long.valueOf(getInput());
+		LOGGER.info("Please enter the address of the customer");
+		String address = getInput();
+		LOGGER.info("Please enter the postcode of the customer");
+		String postcode = getInput();
+		Order order = orderService.update(new Order(orderId, customerId, address, postcode));
+		LOGGER.info("Order Updated");
+		return order;
 	}
 
 	@Override
 	public void delete() {
-		// TODO Auto-generated method stub
-		
+		LOGGER.info("Please enter the id of the order you would like to delete");
+		Long orderid = Long.valueOf(getInput());
+		orderService.delete(orderid);
+		LOGGER.info("Order deleted");
 	}
 
 }
